acc3c90d1a4917ac489448b8fec81b6ea609a48b
Ben Rogers <rogers@farmerrogers.com>
2013-05-03 16:35:21 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RogersFarmerrogersCom::Application.config.secret_token = '9b9d73c7cc4b2d8cf9bf7b2b2d12505b0c95e682f496ea93d3611a101e7f83fa95ffdeac2f299d40c9cc428e98eddf68cd59a3b2bd4690c25b9679826d33675b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RogersFarmerrogersCom::Application.config.session_store :cookie_store, key: '_rogers.farmerrogers.com_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RogersFarmerrogersCom::Application.config.session_store :active_record_store
