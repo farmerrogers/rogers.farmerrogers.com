3925b6eddd2ae12505c08c09889b8b4a038f8b0a
Ben Rogers <rogers@farmerrogers.com>
2013-05-03 17:55:13 -0700

    Adding, editing and initial routing for index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 RogersFarmerrogersCom::Application.routes.draw do
+  get "home/connect"
+
+  get "home/portfolio"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +52,7 @@ RogersFarmerrogersCom::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'Home#index'
 
   # See how all your routes lay out with "rake routes"
 
